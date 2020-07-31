8310a0c1c81a6eee5f957b2783b69cd238b2e0fc
salil0001 <salilsharma.sharma@gmail.com>
2020-08-01 00:17:11 +0530

    Refactored. Dynamic routes feature added.

--- acclegor/index.js
+++ acclegor/index.js
@@ -1,80 +1,60 @@
 const http = require("http");
-const { Stream } = require("stream");
-const HOST_NAME = "127.0.0.1";
-const port = 3000;
-const HEADER_CONTENT_TYPE = "content-type";
-
 
 const createAcclegorServer = () => {
-//   const endPoints = {
-//     "/name": function (req, res) {
-//       res.end("your name is salil ");
-//     },
-//     "/photo": function (req, res) {
-//       res.end("your name is salil ");
-//     },
-//     "/profile": function (req, res) {
-//       res.end("your profile is great");
-//     },
-//     "user-profile": function (req, res) {
-//       if (req.method === "POST") {
-//         res.end("successful ");
-//       } else {
-//         res.end("wrong method");
-//       }
-//     },
-//   };
-
-
-
+  const allCallbacks = {};
+  const fromattedURL = {};
   const getRequest = (path, params, url, hostName) => {
     const makeUrl = new URL(`http://${hostName}${url}`);
-
-
     const pathSplit = path.split("/:");
     //console.log(pathSplit);
     const assignPath = url.split("/");
     //console.log(assignPath);
 
     for (let i = 1; i < pathSplit.length; i++) {
-        if(pathSplit[i]!=="/favicon.ico")
-        params[pathSplit[i]] = assignPath[i];
+      if (pathSplit[i] !== "/favicon.ico") params[pathSplit[i]] = assignPath[i];
     }
   };
 
-  const server = http.Server();
-  server.listen(port, HOST_NAME, () => {
-    console.log(`Server running at http://${HOST_NAME}:${port}/`);
-  });
+  function getCall(path, callback) {
+    const pathSplit = path.split("/:");
+    const paths = {};
+    for (let i = 1; i < pathSplit.length; i++) {
+      paths[pathSplit[i]] = "";
+    }
+    fromattedURL[pathSplit.length - 1] = paths;
+    allCallbacks[pathSplit.length - 1] = callback;
+  }
+  function listenServer(port) {
+    http
+      .createServer(function (req, res) {
+        res.writeHead(200, { "Content-Type": "text/html" }); // http header
 
+        var { url } = req;
 
-  return {
-    get: function (path, callback) {
-      server.on("connection", () => {
-        console.log("connection established");
-      });
-      server.on("request", (req, res) => {
-        // console.log("is req stream ", req instanceof Stream);
-        // console.log("is res stream ", res instanceof Stream);
+        const splitURL = url.split("/");
 
-        const { url } = req;
-
-        // console.log("url => ", url);
-        // console.log("Object => ", Object.getOwnPropertyNames(url));
-        // console.log("Method => ", req.method);
-        // console.log("Params =>  ", req.params);
-
-        if (req.headers[HEADER_CONTENT_TYPE] === "application/json") {
-          if (url in endPoints) {
-            endPoints[url](req, res);
+        if (url === "/favicon.ico") {
+          res.writeHead(200, { "Content-Type": "image/x-icon" });
+          res.end(/* icon content here */);
+        } else {
+          for (let i = 1; i < splitURL.length; i++) {
+            if (splitURL !== "favicon.ico") {
+              let temp = fromattedURL[splitURL.length - 1];
+              temp[Object.keys(temp)[i - 1]] = splitURL[i];
+            }
           }
+          req.params = {};
+          req.params = fromattedURL[splitURL.length - 1];
+          allCallbacks[splitURL.length - 1](req, res);
         }
-
-        req.params={}
-        getRequest(path, req.params, url, req.headers.host);
-        callback(req, res);
+      })
+      .listen(port, function () {
+        console.log(`server start at port ${port}`); //the server object listens on port 3000
       });
-    },
+  }
+  return {
+    get: (path, callback) => getCall(path, callback),
+    listen: (port) => listenServer(port),
   };
 };
 
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const acclegor = require('./acclegor')
 const app= acclegor();
 
-// app.get('/', function (req, res) {  
-//  res.end('<b>Hello world</b>')
-// });
-
 app.get('/:path1', function (req, res) {  
-  res.end(`Hello  <b>${req.params.path1}</b>`)
+  res.write(`/:path1  <b>${req.params.path1}</b> `); //end the response
+  res.end()
+ });
+
+app.get('/:path1/:path2', function (req, res) { 
+  res.write(`/:path1/:path2  <b>${req.params.path1}</b>   <b>${req.params.path2}</b>`); //end the response
+   res.end()
+});
+app.get('/:path1/:path2/:path3', function (req, res) { 
+  res.write(`/:path1/:path2/:path3  <b>${req.params.path1}</b>   <b>${req.params.path2}</b> <b>${req.params.path3}</b>`); //end the response
+   res.end()
 });
 
+
+
+app.listen(3000)
