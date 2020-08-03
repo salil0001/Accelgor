2021a0e6d8be7848a21bf6e5466ef4f1200036db
manojkumardesai <manojkumardbe@gmail.com>
2020-08-03 15:48:27 +0530

    added support for getting login and signup forms

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
 const acclegor = require('./acclegor')
-const app= acclegor();
+const app = acclegor();
 
-app.get('/', function (req, res) {   
-  res.render("./pages/index.html")
- });
-
-app.get('/:path1', function (req, res) {  
-  console.log(req.searchParams)
-  console.log(req.params)
-
-  res.write(`/:path1  <b>${req.params.path1}</b> `); //end the response
-  res.end()
- });
-
-app.get('/:path1/:path2', function (req, res) { 
-  res.write(`/:path1/:path2  <b>${req.params.path1}</b>   <b>${req.params.path2}</b>`); //end the response
-   res.end()
+app.get('/', function(req, res) {
+    res.render("./pages/index.html")
 });
-app.get('/:path1/:path2/:path3', function (req, res) { 
-  res.write(`/:path1/:path2/:path3  <b>${req.params.path1}</b>   <b>${req.params.path2}</b> <b>${req.params.path3}</b>`);
-   res.end()
+
+
+app.get('/:path1', function(req, res) {
+    console.log(req.searchParams)
+    console.log(req.params)
+    if (req.params.path1 === 'signup') {
+        res.render("./pages/signup.html")
+    } else if (req.params.path1 === 'login') {
+        res.render("./pages/login.html")
+    } else {
+        res.write(`Oh Oh, you seem to have typed an invalid url`);
+        res.end();
+    }
+
+    // res.write(`/:path1  <b>${req.params.path1}</b> `); //end the response
+    // res.end()
+});
+
+app.get('/:path1/:path2', function(req, res) {
+    res.write(`/:path1/:path2  <b>${req.params.path1}</b>   <b>${req.params.path2}</b>`); //end the response
+    res.end()
+});
+app.get('/:path1/:path2/:path3', function(req, res) {
+    res.write(`/:path1/:path2/:path3  <b>${req.params.path1}</b>   <b>${req.params.path2}</b> <b>${req.params.path3}</b>`);
+    res.end()
 });
 
 app.listen(3000)
