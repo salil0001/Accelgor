cf05f0ed453cee88c944ea5dc3bf2eb6afc45b0c
manojkumardesai <manojkumardbe@gmail.com>
2020-08-03 16:06:49 +0530

    initial commit with pages

--- acclegor/index.js
+++ acclegor/index.js
@@ -2,82 +2,81 @@ const http = require("http");
 const fs = require("fs");
 
 const createAcclegorServer = () => {
-  const reqResCallbacks = {};
-  const fromattedURL = {};
- 
-  function getCall(path, callback) {
-    const pathSplit = path.split("/:");
-    const paths = {};
+    const reqResCallbacks = {};
+    const fromattedURL = {};
 
-    for (let i = 1; i < pathSplit.length; i++) {
-      paths[pathSplit[i]] = "";
-    }
-    fromattedURL[pathSplit.length - 1] = paths;
-    reqResCallbacks[pathSplit.length - 1] = callback;
-  }
+    function getCall(path, callback) {
+        const pathSplit = path.split("/:");
+        const paths = {};
 
-   /**
-   * for handling params like domain.com?name="hello"
-   */
-  const processedURL=(url)=>{
-    const  newURL=new URL(`https://dummydomain.com${url}`);
-    return{
-        pathname:newURL.pathname,
-        searchParams:Object.fromEntries(newURL.searchParams)
-    }
-  }
-  function listenServer(port) {
-    http
-      .createServer(function (req, res) {
-        res.writeHead(200, { "Content-Type": "text/html" }); // http header
-        var { url } = req;
-        const newURL=processedURL(url);
-        const splitURL = newURL.pathname.split("/");
-
-
-        if (url === "/favicon.ico") {
-          res.writeHead(200, { "Content-Type": "image/x-icon" });
-          res.end(/* icon content here */);
-        } else if (url === "/") {
-          req.params = {};
-          req.searchParams=newURL.searchParams
-
-          res.render = (path) => renderPage(path, req, res);
-          reqResCallbacks[0](req, res);
-        } else {
-          for (let i = 1; i < splitURL.length; i++) {
-            if (splitURL !== "favicon.ico") {
-              let temp = fromattedURL[splitURL.length - 1];
-              temp[Object.keys(temp)[i - 1]] = splitURL[i];
-            }
-          }
-          req.params = {};
-          req.params = fromattedURL[splitURL.length - 1];
-          req.searchParams=newURL.searchParams
-          res.render = (path) => renderPage(path, req, res);
-          reqResCallbacks[splitURL.length - 1](req, res);
+        for (let i = 1; i < pathSplit.length; i++) {
+            paths[pathSplit[i]] = "";
         }
-      })
-      .listen(port, function () {
-        console.log(`server start at port ${port}`); //the server object listens on port 3000
-      });
-  }
+        fromattedURL[pathSplit.length - 1] = paths;
+        reqResCallbacks[pathSplit.length - 1] = callback;
+    }
 
-  function renderPage(path, req, res) {
-    fs.readFile(path, function (err, html) {
-      if (err) {
-        throw err;
-      }
-      res.writeHeader(200, { "Content-Type": "text/html" });
-      res.write(html);
-      res.end();
-    });
-  }
+    /**
+     * for handling params like domain.com?name="hello"
+     */
+    const processedURL = (url) => {
+        const newURL = new URL(`https://dummydomain.com${url}`);
+        return {
+            pathname: newURL.pathname,
+            searchParams: Object.fromEntries(newURL.searchParams)
+        }
+    }
 
-  return {
-    get: (path, callback) => getCall(path, callback),
-    listen: (port) => listenServer(port),
-  };
+    function listenServer(port) {
+        http
+            .createServer(function(req, res) {
+                res.writeHead(200, { "Content-Type": "text/html" }); // http header
+                var { url } = req;
+                const newURL = processedURL(url);
+                const splitURL = newURL.pathname.split("/");
+                if (url === "/favicon.ico") {
+                    res.writeHead(200, { "Content-Type": "image/x-icon" });
+                    res.end( /* icon content here */ );
+                } else if (url === "/") {
+                    req.params = {};
+                    req.searchParams = newURL.searchParams
+
+                    res.render = (path) => renderPage(path, req, res);
+                    reqResCallbacks[0](req, res);
+                } else {
+                    for (let i = 1; i < splitURL.length; i++) {
+                        if (splitURL !== "favicon.ico") {
+                            let temp = fromattedURL[splitURL.length - 1];
+                            temp[Object.keys(temp)[i - 1]] = splitURL[i];
+                        }
+                    }
+                    req.params = {};
+                    req.params = fromattedURL[splitURL.length - 1];
+                    req.searchParams = newURL.searchParams
+                    res.render = (path) => renderPage(path, req, res);
+                    reqResCallbacks[splitURL.length - 1](req, res);
+                }
+            })
+            .listen(port, function() {
+                console.log(`server start at port ${port}`); //the server object listens on port 3000
+            });
+    }
+
+    function renderPage(path, req, res) {
+        fs.readFile(path, function(err, html) {
+            if (err) {
+                throw err;
+            }
+            res.writeHeader(200, { "Content-Type": "text/html" });
+            res.write(html);
+            res.end();
+        });
+    }
+
+    return {
+        get: (path, callback) => getCall(path, callback),
+        listen: (port) => listenServer(port),
+    };
 };
 
 module.exports = createAcclegorServer;
--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const acclegor = require('./acclegor')
 const app = acclegor();
 
+
+// app.get('/signup', function(req, res) {
+//     res.render("./pages/signup.html")
+// });
 app.get('/', function(req, res) {
     res.render("./pages/index.html")
 });
 
 
 app.get('/:path1', function(req, res) {
-    console.log(req.searchParams)
-    console.log(req.params)
-    if (req.params.path1 === 'signup') {
+    // console.log(req.searchParams);
+    // console.log(req.params);
+    console.log('request method', req.method);
+    if (req.method === 'POST') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString(); // convert Buffer to string
+        });
+        req.on('end', () => {
+            console.log(body);
+            res.end('ok');
+        });
+    } else if (req.params.path1 === 'signup') {
         res.render("./pages/signup.html")
     } else if (req.params.path1 === 'login') {
         res.render("./pages/login.html")
