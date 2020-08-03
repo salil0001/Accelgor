d1f54b599f90def7ad8cff3eb567e7bb6219842d
manojkumardesai <manojkumardbe@gmail.com>
2020-08-03 10:38:03 +0530

    formatted code and changed variable names

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const acclegor = require('./acclegor')
-const app= acclegor();
+const app = acclegor();
 
-app.get('/', function (req, res) {   
-  res.render("./pages/index.html")
- });
-
-app.get('/:path1', function (req, res) {  
-  console.log(req.searchParams)
-  console.log(req.params)
-
-  res.write(`/:path1  <b>${req.params.path1}</b> `); //end the response
-  res.end()
- });
-
-app.get('/:path1/:path2', function (req, res) { 
-  res.write(`/:path1/:path2  <b>${req.params.path1}</b>   <b>${req.params.path2}</b>`); //end the response
-   res.end()
+app.get('/', function(req, res) {
+    res.render("./pages/index.html")
 });
-app.get('/:path1/:path2/:path3', function (req, res) { 
-  res.write(`/:path1/:path2/:path3  <b>${req.params.path1}</b>   <b>${req.params.path2}</b> <b>${req.params.path3}</b>`);
-   res.end()
+
+app.get('/:path1', function(req, res) {
+    console.log(req.searchParams)
+    console.log(req.params)
+
+    res.write(`/:path1  <b>${req.params.path1}</b> `); //end the response
+    res.end()
+});
+
+app.get('/:path1/:path2', function(req, res) {
+    res.write(`/:path1/:path2  <b>${req.params.path1}</b>   <b>${req.params.path2}</b>`); //end the response
+    res.end()
+});
+app.get('/:path1/:path2/:path3', function(req, res) {
+    res.write(`/:path1/:path2/:path3  <b>${req.params.path1}</b>   <b>${req.params.path2}</b> <b>${req.params.path3}</b>`);
+    res.end()
 });
 
 app.listen(3000)
